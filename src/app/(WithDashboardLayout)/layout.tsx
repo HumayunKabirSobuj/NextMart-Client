3977a3bb93e2f0521f8bcaab867ad59c6b4ed3ce
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-02-20 14:04:05 +0600

    Sidebar added

--- src/app/(WithDashboardLayout)/layout.tsx
+++ src/app/(WithDashboardLayout)/layout.tsx
@@ -1,6 +1,35 @@
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-    return <div>{children}</div>;
-  };
+import { AppSidebar } from "@/components/modules/dashboard/sidebar/app-sidebar";
+import {
+  Breadcrumb,
   
-  export default DashboardLayout;
-  
+} from "@/components/ui/breadcrumb";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Breadcrumb>
+              
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="min-h-[100vh] md:min-h-min">
+            {children}
+          </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -14,13 +14,20 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { logout } from "@/services/AuthService";
 import { useUser } from "@/context/UserContext";
+import { usePathname, useRouter } from "next/navigation";
+import { protectedRoutes } from "@/constants";
 
 export default function Navbar() {
+  const pathname = usePathname();
+  const router = useRouter();
   const { user, setIsLoading } = useUser();
-  console.log(user);
+  // console.log(user);
   const handleLogOut = () => {
     logout();
     setIsLoading(true);
+    if (protectedRoutes.some((route) => pathname.match(route))) {
+      router.push("/");
+    }
   };
 
   return (
@@ -47,9 +54,7 @@ export default function Navbar() {
           {user ? (
             <>
               <Link href="/create-shop">
-                <Button className="rounded-full" >
-                  Create Shop
-                </Button>
+                <Button className="rounded-full">Create Shop</Button>
               </Link>
               <DropdownMenu>
                 <DropdownMenuTrigger>
