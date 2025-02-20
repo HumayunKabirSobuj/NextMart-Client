209b707d5f09705afcefa1063dc5365fd760b7d8
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-02-20 14:19:14 +0600

    Mofify Sidebar

--- src/app/(WithDashboardLayout)/layout.tsx
+++ src/app/(WithDashboardLayout)/layout.tsx
@@ -1,8 +1,5 @@
 import { AppSidebar } from "@/components/modules/dashboard/sidebar/app-sidebar";
-import {
-  Breadcrumb,
-  
-} from "@/components/ui/breadcrumb";
+
 import {
   SidebarInset,
   SidebarProvider,
@@ -21,14 +18,9 @@ export default function DashboardLayout({
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
-            <Breadcrumb>
-              
-            </Breadcrumb>
           </div>
         </header>
-        <div className="min-h-[100vh] md:min-h-min">
-            {children}
-          </div>
+        <div className="p-4 pt-0 min-h-screen">{children}</div>
       </SidebarInset>
     </SidebarProvider>
   );
